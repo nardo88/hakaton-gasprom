fa21716df0930963d33d94b23970d4d5b468d96f
Maxim Kuznecov <maksimkuznetsov@topverstka.ru>
2021-06-27 06:21:53 +0300

    1

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,33 +1,30 @@
 const passwors = document.getElementById('passwors')
 const login = document.getElementById('login')
 
+
 document.addEventListener('submit', e => {
     e.preventDefault()
 
-    const body = {
-        login: login.value,
-        password: passwors.value
-    }
 
-    fetch('http://e685001fd2d9.ngrok.io/api/v1/auth', {
-            method: 'POST',
-            body: JSON.stringify(body)
-
-        })
+    fetch('../auth.json')
         .then(response => {
             if (response.ok) {
                 return response.json()
-            } else {
-                alert('Неверный логин или пароль');
             }
         })
 
         .then(data => {
             if (data) {
-                passwors.value = ''
-                login.value = ''
-                localStorage.setItem('auth', JSON.stringify(data));
-                location.pathname = 'dushboard.html'
+
+                if (login.value === data.login && passwors.value === data.passwors) {
+                    passwors.value = ''
+                    login.value = ''
+                    localStorage.setItem('auth', JSON.stringify(data));
+                    location.pathname = 'dushboard.html'
+                } else {
+                    alert('Не верный логин или пароль')
+                }
+
 
             }
         })
--- scripts/dushboard.js
+++ scripts/dushboard.js
@@ -1,7 +1,6 @@
 const dushboard = document.querySelector('.dushboard');
 const controlItem = document.querySelectorAll('.control__item');
 const dushboardContentItem = document.querySelectorAll('.dushboard-content__item');
-const dropdown = document.querySelectorAll('.dropdown');
 
 
 const addActive = (array, i , className) => {
@@ -38,30 +37,7 @@ addActive(dushboardContentItem, 0, 'dushboard-content__item--active')
 
 
 
-dropdown.forEach(item => {
-    item.addEventListener('click', e => {
-        const target = e.target.closest('.dropdown__top')
 
-        if (target){
-            const body = target.nextElementSibling;
-            if (body.children[0]){
-
-                const h = body.children[0].clientHeight
-                if (body.clientHeight === 0){
-                    body.style.maxHeight = `${h}px`
-                    target.querySelector('.dropdown__arrow').classList.add('dropdown__arrow--active')
-
-                } else {
-                    body.style.maxHeight = `${0}px`
-                    target.querySelector('.dropdown__arrow').classList.remove('dropdown__arrow--active')
-
-                }
-            }
-
-        }
-    })
-})
-// dropdown__arrow--active
 
 
 // logout
